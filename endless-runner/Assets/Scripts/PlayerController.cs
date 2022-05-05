4178325435d4392dcc8b57515fec4b0431f252bd
mariegof <marie.goffin@gmail.com>
2022-05-05 16:51:02 +0200

    Accélération du jeu activée
    Accélération du jeu activée

--- endless-runner/Assets/Scripts/PlayerController.cs
+++ endless-runner/Assets/Scripts/PlayerController.cs
@@ -2,44 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
     public float moveSpeed;
+    public float speedMultiplier;
+
+    public float speedIncreaseMilestone;
+    private float speedMilstoneCount;
+
+
     public float jumpForce;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
 
     private Rigidbody2D myRigidbody;
 
     public bool grounded;
     public LayerMask whatIsGround;
-    private Collider2D myCollider;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    //private Collider2D myCollider;
 
     private Animator myAnimator;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         myRigidbody = GetComponent<Rigidbody2D>();
 
-        myCollider = GetComponent<Collider2D>();
+        //myCollider = GetComponent<Collider2D>();
 
         myAnimator = GetComponent<Animator>();
+
+        jumpTimeCounter = jumpTime;
+
+        speedMilstoneCount = speedIncreaseMilestone;
     }
 
     // Update is called once per frame
-    void Update() {
-        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+    void Update()
+    {
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround); //return true or false
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if(transform.position.x > speedMilstoneCount)
+        {
+            speedMilstoneCount += speedIncreaseMilestone;
+
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+
+            moveSpeed = moveSpeed * speedMultiplier;
+        }
         
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
-        if(Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))//0=left click, 1 = right, 2 = middle
         {
-            if (grounded)
+            if(grounded)
             {
                 myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
             }
+            
         }
 
-        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
-        myAnimator.SetBool ("Grounded", grounded);
+        if(Input.GetKey (KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            if(jumpTimeCounter > 0)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
 
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+        }
+
+        if(grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
+        myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool ("Grounded", grounded);
     }
 }
