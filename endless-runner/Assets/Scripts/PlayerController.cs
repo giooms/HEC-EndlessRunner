b284de44b7853271790965675609c6cd5a06eece
giooms <gillesooms@outlook.com>
2022-05-15 09:20:44 +0200

    Squashed commit of the following:
    commit 4a2d17ff664f4cf79e7f328502f59d758a6adf1d
    Author: mariegof <marie.goffin@gmail.com>
    Date:   Sat May 14 16:47:15 2022 +0200
    
        Accélération du joueur
    
        Accélération du joueur de 0.05 tous les 50 miles

--- endless-runner/Assets/Scripts/PlayerController.cs
+++ endless-runner/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+    public float speedMultiplier; //multiplicateur de vitesse
+
+    public float speedIncreaseMilestone; //distance à partir de laquelle la vitesse augmente
+    private float speedMilestoneCount; // compteur de distance
+
     public float jumpForce;
 
     private Rigidbody2D myRigidbody;
 
     public bool grounded;
     public LayerMask whatIsGround;
-    private Collider2D myCollider;
+    public Transform groundCheck;
+    public float groundCheckRadius; // a quel point le radius du cercle est grand
+
+    //private Collider2D myCollider;
 
     private Animator myAnimator;
 
@@ -19,14 +27,28 @@ public class PlayerController : MonoBehaviour {
     void Start() {
         myRigidbody = GetComponent<Rigidbody2D>();
 
-        myCollider = GetComponent<Collider2D>();
+        //myCollider = GetComponent<Collider2D>();
 
         myAnimator = GetComponent<Animator>();
+
+        speedMilestoneCount = speedIncreaseMilestone; // Pour que le milstone ne reste pas toujours au point 0
+
     }
 
     // Update is called once per frame
     void Update() {
-        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround); //si le cercle autour du groundcheck touche le sol alors grounded est vrai
+
+        if(transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedIncreaseMilestone;
+
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+            moveSpeed = moveSpeed * speedMultiplier;
+        } //si la position x du joueur est plus grande que que le compteur de distance
         
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
