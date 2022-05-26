b8deb314eb3f17f5dde959293407bb69c2a09094
NathanGeu <nathan.geudens@hotmail.com>
2022-05-26 12:20:10 +0200

    Préparation pour le changement de Player
    Voilà Gilles je pense que ça te facilitera la vie, j'ai déjà associé une fonction qui sera appelée lorsqu'un des joueurs sera "cliqué" (sélectionner).
    Va voir mes commentaires dans le script Main Menu ;)

--- endless-runner/Assets/Scripts/MainMenu.cs
+++ endless-runner/Assets/Scripts/MainMenu.cs
@@ -16,4 +16,12 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void ChoosePlayer()
+    {
+        /* Cette fonction est appelée à chaque fois qu'un des bouttons Player
+        (de PlayerGilles à PlayerNathan) a été cliqué.
+        Il faut écrire dans cette fonction : Desactivate les Player qui n'ont pas été sélectionnés.
+        Ensuite dans le script PlatformGenerator, il faudra aller chercher ligne 73 le nom du GameObject qui est actif. Autre possibilité, mettre les GameObject Player dans un parent, et aller chercher juste le parent dans le script ligne 73?? Vu qu'on prend juste les transform?? => surement le plus facile*/
+    }
 }
